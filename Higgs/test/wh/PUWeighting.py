1194ee46bdee175ee1d1a849eb9ce0d773ee8983
Ram Krishna Dewanjee <ram.krishna.dewanjee@cern.ch>
2012-08-24 10:02:27 -0500

    MODIFIED BY Sudeshna Banerjee TO INCLUDE 'results/FakeRatesEE' DIRECTORY

--- Higgs/test/wh/PUWeighting.py
+++ Higgs/test/wh/PUWeighting.py
@@ -12,15 +12,15 @@ import os
 import ROOT
 ROOT.gSystem.Load('PUWeighting_C')
 
-if '7TeV' in os.environ['jobid']:
-    pu_function = ROOT.weightTrue2011
-else:
-    pu_function = ROOT.weightTrue2012
+#if '7TeV' in os.environ['jobid']:
+#pu_function = ROOT.weightTrue2011
+#else:
+pu_function = ROOT.weightTrue2012
 
 def pu_weight(row):
-    nPU = row.nTruePU
-    if nPU > 0:
-        return pu_function(nPU)
-    else:
+#    nPU = row.nTruePU
+#    if nPU > 0:
+#        return pu_function(nPU)
+#    else:
         return 1.0
 
